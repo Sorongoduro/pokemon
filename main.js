6b1c325e2d9b3582d65174137e01e3616f7bdb2c
lucascapalbo <lolitocomelo@hotmail.com>
2021-12-26 16:31:59 -0300

    commited

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ fetch(`https://pokeapi.co/api/v2/pokemon?limit=1118&offset=0`)
     .then(response => response.json())
     .then(data => data.results.forEach(el => {
         let img = document.createElement('img')
+        let h2 = document.createElement('h2')
+        h2.textContent = el.name.charAt(0).toUpperCase() + el.name.slice(1)
+        container.appendChild(h2)
+
         container.appendChild(img)
         fetch(el.url)
             .then(response => response.json())
